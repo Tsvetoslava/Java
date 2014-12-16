90e6a704a88a28dda7af2457142e59da5d046eea
cceccka <cceccka@abv.bg>
2014-12-16 20:28:37 +0200

    Homework

---
+++ Domashno3/src/Domashno3/SimetrichniChisla2.java
@@ -0,0 +1,31 @@
+package Domashno3;
+
+public class SimetrichniChisla2 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		print();
+	}
+	
+	
+	
+	public static void print(){
+
+		for (int number=0; number<1000; number++) {
+			if(number<10){
+				System.out.print(number+" ");
+			}
+			else if((number>=11 && number<100 && number%10==number/10)){
+             
+				System.out.print(number+" ");
+			}
+			else if(number>100 && number%10==number/100 ){
+				System.out.print(number+" ");
+			}
+		}
+		
+	}
+	
+}

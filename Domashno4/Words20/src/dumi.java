ff1d22b6cee3e79af2284e5868155b6344254430
cceccka <cceccka@abv.bg>
2015-01-07 20:33:50 +0200

    Homework

--- Domashno4/Words20/src/dumi.java
+++ Domashno4/Words20/src/dumi.java
@@ -5,31 +5,35 @@ public class dumi {
 
 	 public static void main(String[] args) {
 		
-	String[] array= new String[4];
+	String[] array= new String[20];
 	Scanner sc=new Scanner(System.in);
+	int counter=1;
 	for (int i = 0; i < array.length; i++) {
-		
+		System.out.println("Vuvedi " +counter +" duma: ");
 		String duma=sc.next();
 		array[i] = new String(duma);
-		
+		counter++;
 }
 	
 	 
-	
- /*   for (int i = 0; i < array.length; i++) {
-        System.out.print(array[i] + " ");
-       if (array[i].equals(array[i + 1])) {
-           System.out.print(array[i]);
-        }   
-    }
-  System.out.print(array[array.length - 1]);
-  
-  */
- 
+	for(int k=0; k<20; k++){
+		
+    	for(int j=0; j<k; j++){
+    		
+    	        if(array[k].equals(array[j])){
+    	        	
+    		        System.out.print(" "+array[k]);
+    	
+    	        }
+    	}  
+	}
+
+    	}
+}
+
 	
 		
-		}  
-		}
-  
+		
+		
   
 
